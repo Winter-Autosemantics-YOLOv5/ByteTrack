69c9722497e176e65744d2bde990fa4769193634
ShinYoung-hwan <syh754200@g.skku.edu>
2022-02-07 14:59:45 +0900

    support yolov5

--- yolox/data/datasets/mot.py
+++ yolox/data/datasets/mot.py
@@ -98,6 +98,7 @@ class MOTDataset(Dataset):
         img_file = os.path.join(
             self.data_dir, self.name, file_name
         )
+        print('imgfile:',img_file)
         img = cv2.imread(img_file)
         assert img is not None
 
